9d384f17bc3ee96815a0d4b7601c7b191a4a93fe
Pavel <pavel@mail.ru>
2024-03-24 12:03:52 +0300

    sem1/lev2/les5 -23_03_2024 end homework

--- gb/sem1/levl2/les5/SumOfArray.java
+++ gb/sem1/levl2/les5/SumOfArray.java
@@ -1,27 +1,79 @@
 package gb.sem1.levl2.les5;
 
+import java.util.Arrays;
+
 public class SumOfArray {
     static final int SIZE = 10_000_000;
     static final int HALF = SIZE / 2;
-    float[] arr = new float[SIZE];
+
 
     public static void main(String[] args) {
+        sumOfArray();
+        sumOfArrayThread();
 
     }
 
     public static void sumOfArray() {
+        float[] arr = new float[SIZE];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = 1.0f;
+        }
         long start = System.currentTimeMillis();
 
-
-
-        System.out.println(System.currentTimeMillis() -start);
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (float) (arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) *
+                    Math.cos(0.4f + i / 2));
+        }
+        System.out.println("One thread "+ (System.currentTimeMillis() - start));
+        System.out.println(arr[SIZE-1]);
     }
 
     public static void sumOfArrayThread() {
+        float[] src = new float[SIZE];
+        for (int i = 0; i < src.length; i++) {
+            src[i] = 1.0f;
+        }
+        float[] dst1  = new float[HALF];
+        float[] dst2  = new float[SIZE - HALF];
+
         long start = System.currentTimeMillis();
 
+        System.arraycopy(src,0,dst1,0,dst1.length);
+        System.arraycopy(src,dst2.length,dst2,0,dst2.length);
 
-        System.out.println(System.currentTimeMillis() -start);
+        Thread thr1 = new Thread(
+                () -> {
+                    for (int i = 0; i < dst1.length; i++) {
+                        dst1[i] = (float) (dst1[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) *
+                                Math.cos(0.4f + i / 2));
+                    }
+                }
+        );
+        Thread thr2 = new Thread(
+                () ->{
+                    for (int i = 0; i < dst2.length; i++) {
+                        int i1 = i + SIZE - HALF;
+                        dst2[i] = (float) (dst2[i] * Math.sin(0.2f + i1  / 5) * Math.cos(0.2f + i1/ 5) *
+                                Math.cos(0.4f + i1 / 2));
+                    }
+                }
+        );
+
+        thr1.start();
+        thr2.start();
+
+        try {
+            thr1.join();
+            thr2.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+
+        System.arraycopy(dst1,0,src,0,HALF);
+        System.arraycopy(dst2,0,src,SIZE- HALF,HALF);
+        System.out.println("With 2 threads "+(System.currentTimeMillis() - start));
+        System.out.println(src[SIZE-1]);
 
     }
 
