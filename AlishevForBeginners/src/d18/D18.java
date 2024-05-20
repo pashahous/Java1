8362f118c970a31eebafad06f074ccb49d5e6910
Pavel <pavel@mail.ru>
2024-05-20 17:10:12 +0300

    19052024

--- AlishevForBeginners/src/d18/D18.java
+++ AlishevForBeginners/src/d18/D18.java
@@ -4,11 +4,21 @@ public class D18 {
 
     public static void main(String[] args) {
         int[] numbers = {1, 10, 1241, 50402, -50, 249, 10215, 665, 2295, 7, 311};
-        System.out.println(sum(numbers,0));
+        System.out.println(recursionSum(numbers, 0));
+        System.out.println(count7(777777));
     }
 
-    private static int sum(int[] arr, int i) {
+    private static int recursionSum(int[] arr, int i) {
         if (i == arr.length) return 0;
-        return arr[i] + sum(arr, i + 1);
+        return arr[i] + recursionSum(arr, i + 1);
     }
+
+    private static int count7(int num) {
+        if (num == 0) return 0;
+        if (num % 10 == 7) {
+            return 1 + count7(num / 10);
+        }
+        return count7(num/10);
+    }
+
 }
---
+++ AlishevForBeginners/src/finalProject/Tile.java
@@ -0,0 +1,19 @@
+package finalProject;
+
+public enum Tile {
+    EMPTY("⬜"),
+    SHIP("\\uD83D\\uDEE5"),
+    SHOT("\\uD83D\\uDFE5"),
+    OREOL ("\\uD83D\\uDFE6 )");
+
+    private Tile(String representation) {
+        this.representation = representation;
+    }
+
+    public String getRepresentation() {
+        return representation;
+    }
+
+    private  String representation;
+
+}
